04942decd70b431b7e248d9eb99c0f3e11e132a4
ReinhardtGao <dragongj002@126.com>
2016-12-29 18:21:47 +0800

    RefTester

---
+++ src/com/java21days/AyeAye.java
@@ -0,0 +1,15 @@
+package com.java21days;
+
+public class AyeAye {
+    int i = 40;
+    int j;
+    public AyeAye(){
+        setValue(i++);
+    }
+
+    void setValue(int inputValue){
+        int i = 20;
+        j = i + 1;
+        System.out.println("j= " + j);
+    }
+}
