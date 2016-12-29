1466fb8fc15ee17d9d1aacf8d1bfb19221ee3f86
ReinhardtGao <dragongj002@126.com>
2016-12-30 00:23:23 +0800

    3-2

--- src/com/java21days/HWD.java
+++ src/com/java21days/HWD.java
@@ -1,9 +1,7 @@
 package com.java21days;
 
 class HWD {
-    public static void main(String[] arguments){
         int height;
         int weight;
         int depth;
-    }
 }
---
+++ src/com/java21days/Sample.java
@@ -0,0 +1,13 @@
+package com.java21days;
+
+public class Sample {
+    public static void main(String[] arguments){
+        HWD eg = new HWD();
+        eg.height = 100;
+        eg.weight = 50;
+        eg.depth = 333;
+        System.out.println("Height is: "+ eg.height);
+        System.out.println("Weight is: "+ eg.weight);
+        System.out.println("Depth is: "+ eg.depth);
+    }
+}
