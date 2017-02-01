e95e215df523c304115c84e3770b9af51a068572
ReinhardtGao <dragongj002@126.com>
2017-02-01 23:26:18 +0800

    Chapter 6

---
+++ src/com/java21days/InstanceCounter.java
@@ -0,0 +1,28 @@
+package com.java21days;
+
+/**
+ * Created by ReinhardtGao on 2017/2/1.
+ */
+public class InstanceCounter {
+    private static int numInstance = 0;
+
+    protected static int getCount(){
+        return numInstance;
+    }
+
+    private static void addInstance(){
+        numInstance++;
+    }
+
+    InstanceCounter(){
+        InstanceCounter.addInstance();
+    }
+
+    public static void main(String[] arguments){
+        System.out.println("Starting with " + InstanceCounter.getCount() + " objects");
+        for (int i = 0; i < 500;++i){
+            new InstanceCounter();
+        }
+        System.out.println("Create " + InstanceCounter.getCount() + " objects");
+    }
+}
